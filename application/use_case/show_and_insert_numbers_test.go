538323e6df4c60a83ea6702eec87e23f65f2ccbe
Cleiton Pereira <cledupe@hotmail.com>
2022-01-17 15:52:19 -0300

    Add Test

--- application/use_case/show_and_insert_numbers_test.go
+++ application/use_case/show_and_insert_numbers_test.go
@@ -6,6 +6,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var Data *[]float64
+
+//Mock de um storage
 type MockDb struct {
 }
 
@@ -13,7 +16,18 @@ func (mockDb MockDb) Load() ([]float64, error) {
 	return []float64{1, 2, 3, 4, 5, 6}, nil
 }
 func (mockDb MockDb) Save(mock []float64) {
+	Data = &mock
+}
 
+//Mock de um provedor de dados
+type MockGetData struct {
+}
+
+func (mockGetData MockGetData) GetData(page int, result chan<- []float64) {
+	if page > 1 {
+		result <- []float64{}
+	}
+	result <- []float64{9, 8, 7, 6, 5, 4, 3, 2, 1}
 }
 
 func TestPagination(t *testing.T) {
@@ -45,5 +59,15 @@ func TestPaginationOutOfElement(t *testing.T) {
 	mockDb := MockDb{}
 	testArray := []float64{}
 	resultArray := ShowNumbers(1, 7, mockDb)
+	assert.Equal(testArray, resultArray, "The Two arrays should be the same")
+}
+
+func TestInsertion(t *testing.T) {
+	assert := assert.New(t)
+	mockDb := MockDb{}
+	mockGetData := MockGetData{}
+	testArray := []float64{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	InsertNumbers(mockGetData, mockDb)
+	resultArray := *Data
 	assert.Equal(testArray, resultArray, "The Two arrays should be the same")
 }
